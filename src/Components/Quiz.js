02581762fc271ca2e9553b72cea682a325bd022b
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-01-20 13:59:14 -0500

    Fixed score display to render on completion of quiz

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Question from './Question.js'
 import Data from './data.js'
 
+//global percent display variable 
+var scorePercentDisplay = "";
+var scoreDisplay = ""
 
 class Quiz extends Component {
 	constructor(props) {
@@ -18,7 +21,7 @@ class Quiz extends Component {
 	userAnswered(correctOrNot) { //exists so that quiz can calculate score and advance question index
 		var newQuestionNumber = this.state.questionIndex + 1; //holding the next question in the index of questions in a variable
 		//need an if statement to see whether or not the answer entered is the correct one.  if it is, update the score, if not advance to next question
-		if (correctOrNot == 1) {
+		if (correctOrNot === 1) {
 			//user got the question right.  Update the score.
 			var newScore = (this.state.userScore + 1);//holds the user's score in a variable
 			console.log(newScore)
@@ -39,12 +42,22 @@ class Quiz extends Component {
 	render () {
 		//quizArray will contain all the questions on the quiz; they need to be pushed on from data.js 
 		var quizArray = [];
-		quizArray.push(
+		if (this.state.questionIndex < Data.questions.length) {
+			quizArray.push(
 			<Question letMeKnowWhenFinished={this.userAnswered} question={Data.questions[this.state.questionIndex]} key={this.state.questionIndex} /> //questions is an array - we have to pass it one question at a time, hence the [] after Data.questions - we need to pass it one question at a time, hence why we're passing it this.state.questionIndex; letMeKnowWhenFinished is the title of the prop that's passed userAnswered (will handle score updating and question advancement once question has been answered and scored)
-		)
+			)
+
+			// var scorePercentDisplay = "3";
+		} else{
+			//the user has completed the quiz
+			scorePercentDisplay = this.state.scoreAsPercent;
+			scoreDisplay = "You got " + scorePercentDisplay + "% correct!"
+
+		}
 		return (
 			<div className="App">
-			<h1>You have {this.state.scoreAsPercent}% correct!</h1>
+
+			<h1>{scoreDisplay}</h1>
 				<h2>{quizArray}</h2>
 			</div>
 		)
