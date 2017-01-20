33b641906a83327f43801cff873a80cfda308ae3
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-20 14:42:20 -0500

    styling start

--- src/Components/SideNavBar.js
+++ src/Components/SideNavBar.js
@@ -8,18 +8,18 @@ import { Link } from 'react-router';
 class SideNavBar extends Component{
     render(){
         return(
-            
-           
-            <div className="col-sm-3">         
-                <nav className="navbar navbar-default stretch-height">                                            
+
+
+            <div className="col-sm-3">
+                <nav className="sideNavMenu navbar navbar-default stretch-height">
                     <Link to="home"> <button className="btn btn-primary button-spread"> Home </button> </Link>
                     <Link to="quizMe"> <button className="btn btn-primary button-spread"> Quiz Me </button> </Link>
                     <Link to="createNewList"> <button className="btn btn-primary button-spread"> Create New List </button> </Link>
-                    <Link to="viewProgress"><button className="btn btn-primary button-spread"> View Progress </button></Link>                    
+                    <Link to="viewProgress"><button className="btn btn-primary button-spread"> View Progress </button></Link>
                 </nav>
             </div>
-            
-            
+
+
         )
     }
 }
