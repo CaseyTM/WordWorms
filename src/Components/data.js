73370b7db2d4a02055a19126bb5afa076c25cea2
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-01-22 15:14:32 -0500

    Modified data.js to include dummy data for random quiz generation

--- src/Components/data.js
+++ src/Components/data.js
@@ -6,38 +6,602 @@ module.exports = {
 		questions: [
 			{
 			id: "1",
-			qtext:"California is in which part of USA?",
+			qtext:"________ is a thin syrup, made from pomegranate juice and often used in mixed drinks.",
 			options:[
-				{text:"East"},
-				{text:"Mid"},
-				{text:"West"},
-				{text:"South"}
+				{text:"Maple"},
+				{text:"Grenadine"},
+				{text:"Agave"},
+				{text:"Huckleberry"}
 			],
-			ans:"West",
+			ans:"Grenadine",
 			marks: 3
 		},
 		{
 			id: "2",
-			qtext:"Who is Prime Minister of India?",
+			qtext:"Characterized by jokes and good humor; funny.",
 			options:[
-				{text:"Sonia Gandhi"},
-				{text:"Narendra Modi"},
-				{text:"Manmohan Singh"},
-				{text:"Rahul Gandhi"}
+				{text:"Dour"},
+				{text:"Glib"},
+				{text:"Jocular"},
+				{text:"Pious"}
 			],
-			ans:"Narendra Modi",
+			ans:"Jocular",
 			marks: 2
 		},
 		{
 			id: "3",
-			qtext:"Which of the following is most popular Search Engine Company?",
+			qtext:"The process of bubbling as gas escapes; the property of giving off bubbles.",
 			options:[
-				{text:"Microsoft"},
-				{text:"Facebook"},
-				{text:"Google"},
-				{text:"Yahoo"}
+				{text:"Sublimation"},
+				{text:"Taxidermy"},
+				{text:"Titration"},
+				{text:"Effervescence"}
 			],
-			ans:"Google",
+			ans:"Effervescence",
+			marks: 1
+		},
+		{
+			id: "4",
+			qtext:"The state of being cancelled or annulled; a formal termination.",
+			options:[
+				{text:"Corroborated"},
+				{text:"Abrogation"},
+				{text:"Reinstatement"},
+				{text:"Disengaged"}
+			],
+			ans:"Abrogation",
+			marks: 1
+		},
+		{
+			id: "5",
+			qtext:"Causing awareness of one's shortcomings; to be made to feel foolish.",
+			options:[
+				{text:"Enlightened"},
+				{text:"Aghast"},
+				{text:"Humiliating"},
+				{text:"Placating"}
+			],
+			ans:"Humiliating",
+			marks: 1
+		},
+		{
+			id: "6",
+			qtext:"The savage and excessive killing of many people.",
+			options:[
+				{text:"Massacre"},
+				{text:"Pilgrimage"},
+				{text:"Retreat"},
+				{text:"Stalemate"}
+			],
+			ans:"Massacre",
+			marks: 1
+		},
+		{
+			id: "7",
+			qtext:"To be profoundly honored; to be held in deep respect and devotion.",
+			options:[
+				{text:"Disparaged"},
+				{text:"Revered"},
+				{text:"Admonished"},
+				{text:"Corroborated"}
+			],
+			ans:"Revered",
+			marks: 1
+		},
+		{
+			id: "8",
+			qtext:"A sudden or abrupt strong increase; a sudden forceful flow.",
+			options:[
+				{text:"Regression"},
+				{text:"Investigation"},
+				{text:"Upsurge"},
+				{text:"Dwindling"}
+			],
+			ans:"Upsurge",
+			marks: 1
+		},
+		{
+			id: "9",
+			qtext:"Possessing an extraordinary ability to attract.",
+			options:[
+				{text:"Cloying"},
+				{text:"Charismatic"},
+				{text:"Insensitive"},
+				{text:"Coy"}
+			],
+			ans:"Charismatic",
+			marks: 1
+		},
+		{
+			id: "10",
+			qtext:"A movement within a country dedicated to overthrowing a government; a rebellion",
+			options:[
+				{text:"Observation"},
+				{text:"Occupation"},
+				{text:"Oligarchy"},
+				{text:"Insurgency"}
+			],
+			ans:"Insurgency",
+			marks: 1
+		},
+		{
+			id: "11",
+			qtext:"Demanding strict attention to rules and procedures.",
+			options:[
+				{text:"Lackadaisical"},
+				{text:"Stringent"},
+				{text:"Relaxed"},
+				{text:"Tempered"}
+			],
+			ans:"Stringent",
+			marks: 1
+		},
+		{
+			id: "12",
+			qtext:"To be ineffective or tactless; clumsy.",
+			options:[
+				{text:"Swaggering"},
+				{text:"Jovial"},
+				{text:"Maladroit"},
+				{text:"Adept"}
+			],
+			ans:"Maladroit",
+			marks: 1
+		},
+		{
+			id: "13",
+			qtext:"To steal or misappropriate funds; to use in a wrong way for one's benefit.",
+			options:[
+				{text:"Embezzle"},
+				{text:"Safeguard"},
+				{text:"Sequester"},
+				{text:"Tithe"}
+			],
+			ans:"Embezzle",
+			marks: 1
+		},
+		{
+			id: "14",
+			qtext:"Including much; comprising many things; having a wide scope or a full view.",
+			options:[
+				{text:"Limited"},
+				{text:"Short-sighted"},
+				{text:"Cloistered"},
+				{text:"Comprehensive"}
+			],
+			ans:"Comprehensive",
+			marks: 1
+		},
+		{
+			id: "15",
+			qtext:"To challenge as false; cast doubt upon.",
+			options:[
+				{text:"Administered"},
+				{text:"Impugned"},
+				{text:"Interrogated"},
+				{text:"Castigated"}
+			],
+			ans:"Impugned",
+			marks: 1
+		},
+		{
+			id: "16",
+			qtext:"Having or showing or arising from a desire to promote the welfare or happiness of others.",
+			options:[
+				{text:"Benevolent"},
+				{text:"Malevolent"},
+				{text:"Indifferent"},
+				{text:"Advocate"}
+			],
+			ans:"Benevolent",
+			marks: 1
+		},
+		{
+			id: "17",
+			qtext:"A group active in the invention and application of new techniques in a given field, especially in the arts.",
+			options:[
+				{text:"Uncouth"},
+				{text:"Tacky"},
+				{text:"Avant-garde"},
+				{text:"Graceful"}
+			],
+			ans:"Avant-garde",
+			marks: 1
+		},
+		{
+			id: "18",
+			qtext:"Open to doubt or suspicion; not convinced.",
+			options:[
+				{text:"Conciliatory"},
+				{text:"Praiseworthy"},
+				{text:"Laudable"},
+				{text:"Dubious"}
+			],
+			ans:"Dubious",
+			marks: 1
+		},
+		{
+			id: "19",
+			qtext:"Subject to, led by, or indicative of a sudden, odd notion or unpredictable change; erratic.",
+			options:[
+				{text:"Boisterous"},
+				{text:"Capricious"},
+				{text:"Stalwart"},
+				{text:"Condemning"}
+			],
+			ans:"Capricious",
+			marks: 1
+		},
+		{
+			id: "20",
+			qtext:"A break or interruption in the continuity of a work, series, action, etc.",
+			options:[
+				{text:"Enlist"},
+				{text:"Reconvene"},
+				{text:"Hiatus"},
+				{text:"Animate"}
+			],
+			ans:"Hiatus",
+			marks: 1
+		},
+		{
+			id: "21",
+			qtext:"A feeling of deep and bitter anger and ill-will.",
+			options:[
+				{text:"Rancor"},
+				{text:"Elation"},
+				{text:"Joy"},
+				{text:"Indifference"}
+			],
+			ans:"Rancor",
+			marks: 1
+		},
+		{
+			id: "22",
+			qtext:"An unpaid overdue debt; a state of being in debt.",
+			options:[
+				{text:"Excessive"},
+				{text:"Withdrawal"},
+				{text:"Arrears"},
+				{text:"Malfeasance"}
+			],
+			ans:"Arrears",
+			marks: 1
+		},
+		{
+			id: "23",
+			qtext:"A corrupt or depraved or degenerate act or practice.",
+			options:[
+				{text:"Appreciative"},
+				{text:"Turpitude"},
+				{text:"Benefactor"},
+				{text:"Instigative"}
+			],
+			ans:"Turpitude",
+			marks: 1
+		},
+		{
+			id: "24",
+			qtext:"A difference between conflicting facts or claims or opinions.",
+			options:[
+				{text:"Discrepancy"},
+				{text:"Consistent"},
+				{text:"Similar"},
+				{text:"Convergent"}
+			],
+			ans:"Discrepancy",
+			marks: 1
+		},
+		{
+			id: "25",
+			qtext:"A carrying away of a person against his will, or illegally.",
+			options:[
+				{text:"Reinstatement"},
+				{text:"Abduction"},
+				{text:"Appropriate"},
+				{text:"Cornering"}
+			],
+			ans:"Abduction",
+			marks: 1
+		},
+		{
+			id: "26",
+			qtext:"To indulge in profane oaths.",
+			options:[
+				{text:"Convict"},
+				{text:"Censure"},
+				{text:"Expel"},
+				{text:"Blaspheme"}
+			],
+			ans:"Blaspheme",
+			marks: 1
+		},
+		{
+			id: "27",
+			qtext:"To mention, for the first time.",
+			options:[
+				{text:"Broach"},
+				{text:"Advance"},
+				{text:"Moot"},
+				{text:"Tender"}
+			],
+			ans:"Broach",
+			marks: 1
+		},
+		{
+			id: "28",
+			qtext:"Somewhat rough or rude in manner or speech.",
+			options:[
+				{text:"Honest"},
+				{text:"Candid"},
+				{text:"Brusque"},
+				{text:"Sincere"}
+			],
+			ans:"Brusque",
+			marks: 1
+		},
+		{
+			id: "29",
+			qtext:"Any support or prop.",
+			options:[
+				{text:"Pier"},
+				{text:"Buttress"},
+				{text:"Rail"},
+				{text:"Vice"}
+			],
+			ans:"Buttress",
+			marks: 1
+		},
+		{
+			id: "30",
+			qtext:"A disagreeable, harsh, or discordant sound or combination of sounds or tones.",
+			options:[
+				{text:"Sonnet"},
+				{text:"Lilt"},
+				{text:"Cacophony"},
+				{text:"Opus"}
+			],
+			ans:"Cacophony",
+			marks: 1
+		},
+		{
+			id: "31",
+			qtext:"To impose on or dupe by flattering speech.",
+			options:[
+				{text:"Cajole"},
+				{text:"Attract"},
+				{text:"Charm"},
+				{text:"Wile"}
+			],
+			ans:"Cajole",
+			marks: 1
+		},
+		{
+			id: "32",
+			qtext:"The quality of frankness or outspokenness.",
+			options:[
+				{text:"Discretion"},
+				{text:"Tolerance"},
+				{text:"Candor"},
+				{text:"Moderation"}
+			],
+			ans:"Candor",
+			marks: 1
+		},
+		{
+			id: "33",
+			qtext:"A picture or description in which natural characteristics are exaggerated or distorted.",
+			options:[
+				{text:"Emulate"},
+				{text:"Caricature"},
+				{text:"Travesty"},
+				{text:"Animation"}
+			],
+			ans:"Caricature",
+			marks: 1
+		},
+		{
+			id: "34",
+			qtext:"A charge for a firearm, or for blasting.",
+			options:[
+				{text:"Emulate"},
+				{text:"Baggage"},
+				{text:"Cartridge"},
+				{text:"Envelope"}
+			],
+			ans:"Cartridge",
+			marks: 1
+		},
+		{
+			id: "35",
+			qtext:"An official numbering of the people of a country or district.",
+			options:[
+				{text:"Census"},
+				{text:"Arrangement"},
+				{text:"Menu"},
+				{text:"Gazette"}
+			],
+			ans:"Census",
+			marks: 1
+		},
+		{
+			id: "36",
+			qtext:"Discontinuance, as of action or motion.",
+			options:[
+				{text:"Progression"},
+				{text:"Admission"},
+				{text:"Torrent"},
+				{text:"Cessation"}
+			],
+			ans:"Cessation",
+			marks: 1
+		},
+		{
+			id: "37",
+			qtext:"A yawning hollow, as in the earth's surface.",
+			options:[
+				{text:"Smooth"},
+				{text:"Chasm"},
+				{text:"Glove"},
+				{text:"Seamless"}
+			],
+			ans:"Chasm",
+			marks: 1
+		},
+		{
+			id: "38",
+			qtext:"Belonging, relating to, or abounding in color.",
+			options:[
+				{text:"Chromatic"},
+				{text:"Picturesque"},
+				{text:"Uninteresting"},
+				{text:"Motley"}
+			],
+			ans:"Chromatic",
+			marks: 1
+		},
+		{
+			id: "39",
+			qtext:"Intuitive sagacity or perception.",
+			options:[
+				{text:"Anticipation"},
+				{text:"Caution"},
+				{text:"Discretion"},
+				{text:"Clairvoyance"}
+			],
+			ans:"Clairvoyance",
+			marks: 1
+		},
+		{
+			id: "40",
+			qtext:"The change of endings in nouns and adj. to express their different relations of gender.",
+			options:[
+				{text:"Intemperance"},
+				{text:"Declension"},
+				{text:"Lewdness"},
+				{text:"Debasement"}
+			],
+			ans:"Declension",
+			marks: 1
+		},
+		{
+			id: "41",
+			qtext:"To cut off or take away, as a part of something.",
+			options:[
+				{text:"Abstract"},
+				{text:"Dishonor"},
+				{text:"Defalcate"},
+				{text:"Defraud"}
+			],
+			ans:"Defalcate",
+			marks: 1
+		},
+		{
+			id: "42",
+			qtext:"That part of a play or story in which the mystery is cleared up.",
+			options:[
+				{text:"Calamity"},
+				{text:"Denouement"},
+				{text:"Havoc"},
+				{text:"Debacle"}
+			],
+			ans:"Denouement",
+			marks: 1
+		},
+		{
+			id: "43",
+			qtext:"Any remedy which, when applied externally, dries up or absorbs moisture, as that of wounds.",
+			options:[
+				{text:"Desiccant"},
+				{text:"Humid"},
+				{text:"Depleted"},
+				{text:"Exhausted"}
+			],
+			ans:"Desiccant",
+			marks: 1
+		},
+		{
+			id: "44",
+			qtext:"Not connected with what precedes.",
+			options:[
+				{text:"Thoughtless"},
+				{text:"Accidental"},
+				{text:"Organized"},
+				{text:"Desultory"}
+			],
+			ans:"Desultory",
+			marks: 1
+		},
+		{
+			id: "45",
+			qtext:"A positive utterance.",
+			options:[
+				{text:"Motto"},
+				{text:"Dictum"},
+				{text:"Proverb"},
+				{text:"Axiom"}
+			],
+			ans:"Dictum",
+			marks: 1
+		},
+		{
+			id: "46",
+			qtext:"A noxious or ill-smelling exhalation from decaying or putrefying matter.",
+			options:[
+				{text:"Emission"},
+				{text:"Diffusion"},
+				{text:"Effluvium"},
+				{text:"Discharge"}
+			],
+			ans:"Effluvium",
+			marks: 1
+		},
+		{
+			id: "47",
+			qtext:"To endow with a privilege, especially with the right to vote.",
+			options:[
+				{text:"Enfranchise"},
+				{text:"Restrain"},
+				{text:"Deliver"},
+				{text:"Redeem"}
+			],
+			ans:"Enfranchise",
+			marks: 1
+		},
+		{
+			id: "48",
+			qtext:"Hatred; the state or feeling of being actively opposed or hostile to someone or something.",
+			options:[
+				{text:"Alienation"},
+				{text:"Estrangement"},
+				{text:"Annoyance"},
+				{text:"Enmity"}
+			],
+			ans:"Enmity",
+			marks: 1
+		},
+		{
+			id: "49",
+			qtext:"Worthy of respect.",
+			options:[
+				{text:"Excellent"},
+				{text:"Estimable"},
+				{text:"Ascertainable"},
+				{text:"Substantial"}
+			],
+			ans:"Estimable",
+			marks: 1
+		},
+		{
+			id: "50",
+			qtext:"Done or made without much or any preparation.",
+			options:[
+				{text:"Fortuitous"},
+				{text:"Expedient"},
+				{text:"Premeditated"},
+				{text:"Extemporaneous"}
+			],
+			ans:"Extemporaneous",
 			marks: 1
 		},
 		]
