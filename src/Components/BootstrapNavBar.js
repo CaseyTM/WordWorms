2a21d6cd89cb3b63beb151174626d446a71e5f78
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-23 16:34:21 -0500

    more styling

--- src/Components/BootstrapNavBar.js
+++ src/Components/BootstrapNavBar.js
@@ -30,7 +30,8 @@ class BootstrapNavBar extends Component{
 			<div className="row">
 				<div className="col-sm-12">
 					<nav className="navbar navbar-default ww-nav">
-						<img src="http://www.clipartkid.com/images/8/cute-bookworm-svg-cut-file-cute-bookworm-clipart-free-svgs-free-svg-LMlbUC-clipart.png" />
+						{/*<img src="http://www.clipartkid.com/images/8/cute-bookworm-svg-cut-file-cute-bookworm-clipart-free-svgs-free-svg-LMlbUC-clipart.png" />*/}
+						<div className="searchWord col-sm-2 col-md-6 col-lg-8 nav-fix">WordWorms</div>
 
 
 						<form onSubmit={this.handleSearchSubmit} className="navbar-right col-sm-10 col-md-6 col-lg-4 form-inline">
