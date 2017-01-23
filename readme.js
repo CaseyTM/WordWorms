880faf75a23ba45185b99ad430d13b9cc54dafc8
Casey McCaskill <caseymccaskill@Caseys-MacBook-Pro.local>
2017-01-22 22:26:48 -0500

    Adding Readme file

---
+++ readme.js
@@ -0,0 +1,45 @@
+WordWorms
+
+Link to LIVE DEMO
+
+Contents
+
+	What It Is
+	What We Used
+	Challenges and Solutions
+	MVP
+	Our Stretch Goals
+	Authors
+	Screenshots
+	Github Link
+	Code Examples
+
+What it is
+	This is our first collaborative front-end project which utilizes our knowledge gained thus far.  We desired 
+	an environment where the user could expand their TALKIN' SKILLZ WIF WORDS in a responsive environment. The app 
+	provides some information to begin however it is designed to provide a repository for unfamiliar terms, ensuring
+	you never need to look up a word more than once.
+
+What we Used
+	React and React-Router
+	Javascript ES-6
+	HTML
+	CSS
+	Jquery
+	The worst APIs in the world
+
+Challenges and Solutions
+	Our only regret is that we had but one Rishi!
+
+MVP
+
+Our Stretch Goals 
+
+Authors
+
+Screen shots
+
+Github
+
+Code Example
+
